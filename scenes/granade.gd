103a31c8607ad51bb9530b1eeb42c90edc40a241
Jonathan Hecl <jonathanhecl@gmail.com>
2023-11-13 02:11:25 -0300

    Granade

---
+++ scenes/granade.gd
@@ -0,0 +1,50 @@
+extends Node2D
+
+const GRAVITY = 0.1
+
+var dir: Vector2
+
+func init(_dir: Vector2):
+	dir = _dir.normalized() * max(2, _dir.length() * 0.01)
+	
+func _process(delta):
+	$Label.text = str(ceil($Timer.time_left))
+	
+func _physics_process(delta):
+	if $Timer.time_left <= 0:
+		return
+	dir.y += GRAVITY / max(dir.length(), 1)
+	move_granade()
+	pass
+	
+func move_granade():
+	var velocity = dir
+	while abs(velocity.y) > 0:
+		var new_position = position + (Vector2.DOWN * sign(velocity.y) * min(abs(velocity.y), 1.0))
+		var normal = get_parent().current_map.collision_normal(new_position)
+		velocity.y -= min(1.0, abs(velocity.y)) * sign(velocity.y)
+		if normal == Vector2.ONE:
+			break
+		if sign(normal.y) != 0 and sign(dir.y) != sign(normal.y):
+			dir.y *= -0.5
+			velocity.y *= -0.5
+		if sign(normal.x) != 0 and sign(dir.x) != sign(normal.x):
+			dir.x *= -0.8
+			velocity.x *= -0.8
+		position = new_position
+	while abs(velocity.x) > 0:
+		var new_position = position + (Vector2.RIGHT * sign(velocity.x) * min(abs(velocity.x), 1.0))
+		var normal = get_parent().current_map.collision_normal(new_position)
+		velocity.x -= min(1.0, abs(velocity.x)) * sign(velocity.x)
+		if normal == Vector2.ONE:
+			break
+		if sign(normal.y) != 0 and sign(dir.y) != sign(normal.y):
+			dir.y *= -0.5
+			velocity.y *= -0.5
+		if sign(normal.x) != 0 and sign(dir.x) != sign(normal.x):
+			dir.x *= -0.8
+			velocity.x *= -0.8
+		position = new_position
+	
+func _on_timer_timeout():
+	self.queue_free()
--- scenes/player.gd
+++ scenes/player.gd
@@ -1,28 +1,33 @@
 extends Node2D
 
+var res_granade = preload("res://scenes/granade.tscn")
+
 var jump = 0
 var jump_dir = 0
+var current_map
+
+func _ready():
+	current_map = get_parent().current_map
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.pressed:
-		#var grenade = Grenade.instance()
-		#grenade.position = position + Vector2.UP * 12
-		#grenade.init(get_global_mouse_position() - global_position + Vector2.UP * 12)
-		#get_parent().add_child(grenade)
-		pass
+		var grenade = res_granade.instantiate()
+		grenade.position = position + Vector2.UP * 12
+		grenade.init(get_global_mouse_position() - global_position + Vector2.UP * 12)
+		get_parent().add_child(grenade)
 
 func _physics_process(delta: float) -> void:
 	if jump > 0:
 		var valid_pos = position
 		for dir in [Vector2(jump_dir, 0), Vector2(jump_dir, -1), Vector2(jump_dir, -2), Vector2(jump_dir, -3), Vector2(jump_dir, -4), Vector2(jump_dir, -5)]:
 			var pos = (position + dir) 
-			if get_parent().current_map.collision_normal(pos) == Vector2.ZERO:
+			if current_map.collision_normal(pos) == Vector2.ZERO:
 				valid_pos = pos
-		jump -= 1
+		jump -= 0.5
 		position = valid_pos
 		return
 	var walk = jump_dir
-	if get_parent().current_map.collision_normal(position + Vector2.DOWN) != Vector2.ZERO:
+	if current_map.collision_normal(position + Vector2.DOWN) != Vector2.ZERO:
 		jump_dir = 0
 		if Input.is_action_just_pressed("ui_accept"):
 			if jump == 0:
@@ -32,6 +37,6 @@ func _physics_process(delta: float) -> void:
 	var valid_pos = position
 	for dir in [Vector2(walk, -3), Vector2(walk, -2), Vector2(walk, -1), Vector2(walk, 0), Vector2(walk, 1), Vector2(walk, 2), Vector2(walk, 3)]:
 		var pos = (position + dir) 
-		if get_parent().current_map.collision_normal(pos) == Vector2.ZERO:
+		if current_map.collision_normal(pos) == Vector2.ZERO:
 			valid_pos = pos
 	position = valid_pos
